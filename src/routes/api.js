658fae8d1b8aeebf27ff3b23cecadc78c64ee6e8
Akbaresa <takutoakbar354@gmail.com>
2024-08-04 19:14:45 +0700

    add api for create room allow all and with password

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,14 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
+import roomController from "../controller/room-controller.js";
 
 const headerRouter = new express.Router();
 
 headerRouter.use(authMiddleware);
 headerRouter.get("/api/users/current" , userController.get);
+headerRouter.post("/api/room/create-room-allow-all", roomController.createRoomAllowAll);
+headerRouter.post("/api/room/create-room-password", roomController.createRoomWithPassword);
 
 export {
     headerRouter
