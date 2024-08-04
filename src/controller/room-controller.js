a5e65549fd058de7df52c83aa526cf421812d1fc
Akbaresa <takutoakbar354@gmail.com>
2024-08-04 19:14:23 +0700

    add controller create room allow all and with passsword

---
+++ src/controller/room-controller.js
@@ -0,0 +1,39 @@
+import { ResponseError } from "../error/response-error.js";
+import roomService from "../service/room-service.js";
+
+
+const createRoomAllowAll = async (req , res , next) => {
+    try {
+        const result = await roomService.createRoomAllowAll();
+        res.status(200).json({
+            data: result
+        });
+    }catch(e){
+        if (e instanceof ResponseError) {
+            res.status(e.status).json(e.toJSON());
+        } else {
+            next(e);
+        }
+    }
+}
+
+const createRoomWithPassword = async (req, res, next) => {
+    try {
+        const password = req.body.password;
+        const result = await roomService.createRoomWithPassword(password);
+        return res.status(200).json({
+            data: result
+        });
+    }catch(e){
+        if (e instanceof ResponseError) {
+            res.status(e.status).json(e.toJSON());
+        } else {
+            next(e);
+        }
+    }
+}
+
+export default {
+    createRoomAllowAll,
+    createRoomWithPassword
+}
