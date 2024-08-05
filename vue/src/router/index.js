a8a379bb6ebac6de6a7cdcd3e329513eec0c6da1
Akbaresa <takutoakbar354@gmail.com>
2024-08-05 10:27:59 +0700

    add route and child route for admin

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,23 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginRoom from '../views/LoginRoom.vue';
+import LoginRoom from '../views/Login.vue';
+import Register from '../views/Register.vue';
 import Room from '../views/Room.vue';
+import Home from '@/components/Home.vue'
+import { getTokenCookie } from "@/api/cookie";
+import Main from '@/views/Main.vue';
+import Event from '@/components/Event.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
+    { 
+      path: '/login', 
+      component: LoginRoom 
+    },
+    { 
+      path: '/register', 
+      component: Register 
+    },
+    { 
+      path: '/room/:roomId', 
+      component: Room 
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path : '/',
+      name: 'beranda',
+      component: Main,
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home,
+        },
+        {
+          path: '/event',
+          name: 'event',
+          component: Event,
+        },
+      ],
     },
-    { path: '/login-room', component: LoginRoom },
-    { path: '/room/:roomId', component: Room }
   ]
-})
+});
 
+const authMiddleware = (to, from, next) => {
+  const isAuthenticated = getTokenCookie(); 
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isAuthenticated) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+};
+
+
+router.beforeEach(authMiddleware);
 export default router
