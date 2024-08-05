8cef6e9398508a666db3c1bc479688b0aa995829
Akbaresa <takutoakbar354@gmail.com>
2024-08-05 10:19:07 +0700

    add vue toastivy to main

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,9 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Toast , { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 const app = createApp(App)
 
+app.use(Toast,{
+    position: POSITION.TOP_RIGHT,
+    timeout: 3000,
+    transition: "Vue-Toastification__fade",
+    maxToasts: 20,
+    newestOnTop: true
+});
 app.use(router)
-
 app.mount('#app')
