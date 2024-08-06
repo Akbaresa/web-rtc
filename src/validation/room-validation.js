a80e155d422a3572fec93785783168143476f889
Akbaresa <takutoakbar354@gmail.com>
2024-08-06 21:42:54 +0700

    add room validation

--- src/validation/room-validation.js
+++ src/validation/room-validation.js
@@ -1,7 +1,16 @@
 import Joi from "joi";
 
-const createRoomWithPasswordValidation = Joi.string().max(100).required()
+const createRoomWithPasswordValidation = Joi.string().max(100).required();
+
+const joinUserRoomValidation = Joi.object({
+    userId: Joi.string().max(100).required(),
+    roomId: Joi.string().max(100).required()
+});
+
+const getRoomByKodeValidation = Joi.string().max(100).required();
 
 export {
-    createRoomWithPasswordValidation
+    createRoomWithPasswordValidation,
+    joinUserRoomValidation,
+    getRoomByKodeValidation
 }
