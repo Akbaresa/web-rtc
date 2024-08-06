5ebd6b67055ce55d719b2616334ced36dc8217cd
Akbaresa <takutoakbar354@gmail.com>
2024-08-06 21:48:11 +0700

    add api header service for interceptor

--- vue/src/api/apiService.js
+++ vue/src/api/apiService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getTokenCookie } from './cookie';
 
 export const apiService = axios.create({
   baseURL: 'http://localhost:3000/api/',
@@ -6,3 +7,19 @@ export const apiService = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export const apiHeaderService = axios.create({
+  baseURL: 'http://localhost:3000/api/',
+});
+
+apiHeaderService.interceptors.request.use(
+  (config) => {
+    const token = getTokenCookie();
+    config.headers['Authorization'] = token;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
