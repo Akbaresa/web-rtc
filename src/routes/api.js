b12b4a93ed36fd3e785980be7abff3df37274ca0
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 08:09:31 +0700

    add route get all user room by kode room

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 import roomController from "../controller/room-controller.js";
+import userRoomController from "../controller/user-room-controller.js";
 
 const headerRouter = new express.Router();
 
@@ -11,6 +12,7 @@ headerRouter.post("/api/room/create-room-allow-all", roomController.createRoomAl
 headerRouter.post("/api/room/create-room-password", roomController.createRoomWithPassword);
 headerRouter.get("/api/room/get-room", roomController.getRoomByKode);
 headerRouter.post("/api/room/join", roomController.joinUserRoom);
+headerRouter.post("/api/all-user-room", userRoomController.getAllUserByKodeRoom);
 
 export {
     headerRouter
