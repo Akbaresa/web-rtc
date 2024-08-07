e3920ee7c75471acefc2badbd33a03cfae134f28
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 15:58:19 +0700

    delete import join user room

--- src/service/room-service.js
+++ src/service/room-service.js
@@ -1,6 +1,6 @@
 import db from "../app/database.js";
 import { validate } from "../validation/validation.js";
-import { createRoomWithPasswordValidation, getRoomByKodeValidation, joinUserRoomValidation } from "../validation/room-validation.js";
+import { createRoomWithPasswordValidation, getRoomByKodeValidation } from "../validation/room-validation.js";
 import { generateRoomCode } from "../helper/room-helper.js";
 import { hashBcrypt } from "../helper/security-helper.js";
 import { ResponseError } from "../error/response-error.js";
@@ -43,26 +43,9 @@ const getRoomByKode = async (kode) => {
     return roomRows[0];
 }
 
-const joinUserRoom = async (request) => {
-    const userRoom = validate(joinUserRoomValidation, request);
-
-    const [countUserRoom] = await db.query('SELECT COUNT(*) as count FROM user_room WHERE user_id = ? AND room_id = ?', [userRoom.userId, userRoom.roomId]);
-
-    if(countUserRoom[0].count === 1) {
-        throw new ResponseError(400, "User sudah masuk");
-    }
-
-    await db.query('INSERT INTO user_room (user_id, room_id) VALUES (?, ?)', [userRoom.userId , userRoom.roomId]);
-
-    return {
-        userId: userRoom.userId,
-        roomId: userRoom.roomId
-    }
-}
 
 export default {
     createRoomAllowAll,
     createRoomWithPassword,
-    joinUserRoom,
     getRoomByKode
 }
