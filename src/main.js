d30a141d74deae7e887b4321a5223ab58785c053
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 08:11:49 +0700

    run  server for socket

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { logger } from "./app/logging.js";
-import { web } from "./app/web.js";
+import { web, server } from "./app/web.js";
 
 web.listen(3000, () => {
+    logger.info("Aplikasi sedang berjalan");
+});
+server.listen(4000, () => {
     logger.info("Aplikasi sedang berjalan");
 });
