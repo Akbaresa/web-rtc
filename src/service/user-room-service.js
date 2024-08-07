4e5c0591915b382d934b53344916be4c056ad737
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 15:58:43 +0700

    add join user room by kode and username in user room service

--- src/service/user-room-service.js
+++ src/service/user-room-service.js
@@ -3,7 +3,7 @@ import { validate } from "../validation/validation.js";
 import { generateRoomCode } from "../helper/room-helper.js";
 import { hashBcrypt } from "../helper/security-helper.js";
 import { ResponseError } from "../error/response-error.js";
-import { getAllUserRoomValidation } from "../validation/user-room.validation.js";
+import { getAllUserRoomValidation, joinUserRoomValidation } from "../validation/user-room.validation.js";
 import roomService from "../service/room-service.js";
 import { logStringify } from "../helper/log-helper.js";
 
@@ -19,6 +19,30 @@ const getAllUserRoom = async (kode) => {
     return userRoomRow;
 }
 
+const joinUserRoom = async (request) => {
+    const userRoom = validate(joinUserRoomValidation, request);
+
+    const room = await db.query('SELECT id_room FROM room WHERE kode = ?', [userRoom.kode]);
+    const user = await db.query('SELECT id_user FROM users WHERE username = ? ', [userRoom.username]);
+    
+    const roomId = room[0][0].id_room;
+    const userId = user[0][0].id_user;
+
+    const [countUserRoom] = await db.query('SELECT COUNT(*) as count FROM user_room WHERE user_id = ? AND room_id = ?', [userId, roomId]);
+
+    if(countUserRoom[0].count === 1) {
+        throw new ResponseError(400, "User sudah masuk");
+    }
+
+    await db.query('INSERT INTO user_room (user_id, room_id) VALUES (?, ?)', [userId , roomId]);
+
+    return {
+        userId: userId,
+        roomId: roomId
+    }
+}
+
 export default {
-    getAllUserRoom
+    getAllUserRoom,
+    joinUserRoom
 }
