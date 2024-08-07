7cb6e321463fca54e982485047d697951c797d0c
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 08:08:20 +0700

    add user room controller for get all user room by kode room

---
+++ src/controller/user-room-controller.js
@@ -0,0 +1,21 @@
+import { ResponseError } from "../error/response-error.js";
+import userRoomService from "../service/user-room-service.js";
+
+const getAllUserByKodeRoom = async (req, res, next) => {
+    try {
+        const result = await userRoomService.getAllUserRoom(req.body.kode);
+        return res.status(200).json({
+            data: result
+        })
+    }catch (e) {
+        if (e instanceof ResponseError) {
+            res.status(e.status).json(e.toJSON());
+        } else {
+            next(e);
+        }
+    }
+}
+
+export default {
+    getAllUserByKodeRoom
+}
