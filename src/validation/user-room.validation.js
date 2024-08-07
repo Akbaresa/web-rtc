3ff3997762734c636e7b0059702eb7ed23396d1c
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 15:56:08 +0700

    add user room validation for join user room by username and kode

--- src/validation/user-room.validation.js
+++ src/validation/user-room.validation.js
@@ -2,6 +2,12 @@ import Joi from "joi";
 
 const getAllUserRoomValidation = Joi.string().max(100).required();
 
+const joinUserRoomValidation = Joi.object({
+    username: Joi.string().max(100).required(),
+    kode: Joi.string().max(100).required()
+});
+
 export {
-    getAllUserRoomValidation
+    getAllUserRoomValidation,
+    joinUserRoomValidation
 }
