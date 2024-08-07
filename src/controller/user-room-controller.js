344daa8fa787c1ce07c0e16f2912be656fe580a6
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 15:57:46 +0700

    add join user room

--- src/controller/user-room-controller.js
+++ src/controller/user-room-controller.js
@@ -16,6 +16,22 @@ const getAllUserByKodeRoom = async (req, res, next) => {
     }
 }
 
+const joinUserRoom = async (req, res, next) => {
+    try {
+        const result = await userRoomService.joinUserRoom(req.body);
+        return res.status(200).json({
+            data: result
+        });
+    }catch(e){
+        if (e instanceof ResponseError) {
+            res.status(e.status).json(e.toJSON());
+        } else {
+            next(e);
+        }
+    }
+}
+
 export default {
-    getAllUserByKodeRoom
+    getAllUserByKodeRoom,
+    joinUserRoom
 }
