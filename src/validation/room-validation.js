e38dd3df9229ccf9688dd994c4cbf8f4f158f505
Akbaresa <takutoakbar354@gmail.com>
2024-08-07 15:56:37 +0700

    change to user room validation

--- src/validation/room-validation.js
+++ src/validation/room-validation.js
@@ -2,15 +2,9 @@ import Joi from "joi";
 
 const createRoomWithPasswordValidation = Joi.string().max(100).required();
 
-const joinUserRoomValidation = Joi.object({
-    userId: Joi.string().max(100).required(),
-    roomId: Joi.string().max(100).required()
-});
-
 const getRoomByKodeValidation = Joi.string().max(100).required();
 
 export {
     createRoomWithPasswordValidation,
-    joinUserRoomValidation,
     getRoomByKodeValidation
 }
