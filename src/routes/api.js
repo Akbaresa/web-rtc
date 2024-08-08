2b502ab2734d2481df710c990298ce8d05bba543
Akbaresa <takutoakbar354@gmail.com>
2024-08-08 10:08:20 +0700

    refactor /api to web

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,12 +7,12 @@ import userRoomController from "../controller/user-room-controller.js";
 const headerRouter = new express.Router();
 
 headerRouter.use(authMiddleware);
-headerRouter.get("/api/users/current" , userController.get);
-headerRouter.post("/api/room/create-room-allow-all", roomController.createRoomAllowAll);
-headerRouter.post("/api/room/create-room-password", roomController.createRoomWithPassword);
-headerRouter.get("/api/room/get-room", roomController.getRoomByKode);
-headerRouter.post("/api/room/join", userRoomController.joinUserRoom);
-headerRouter.post("/api/all-user-room", userRoomController.getAllUserByKodeRoom);
+headerRouter.get("/users/current" , userController.get);
+headerRouter.post("/room/create-room-allow-all", roomController.createRoomAllowAll);
+headerRouter.post("/room/create-room-password", roomController.createRoomWithPassword);
+headerRouter.get("/room/get-room", roomController.getRoomByKode);
+headerRouter.post("/room/join", userRoomController.joinUserRoom);
+headerRouter.post("/all-user-room", userRoomController.getAllUserByKodeRoom);
 
 export {
     headerRouter
